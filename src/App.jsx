abd39630c54ab91d922d0c41ebac0d2cea213737
n17y17 <n17y17@xmailer.be>
2023-06-19 19:22:36 +0900

    Todoの配列を管理するための状態を作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function App() {
   const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState(["掃除", "洗濯", "買い物"]);
 
   function handleChange(event) {
     setTodoInput(event.target.value);
@@ -10,9 +11,9 @@ function App() {
   return (
     <div>
       <input type="text" value={todoInput} onChange={handleChange} />
-      <p>{todoInput}</p>
+      {todos.map((todo, i) => <p key={i}>{todo}</p>)}
     </div>
   );
 };
 
-export default App;
+export default App
